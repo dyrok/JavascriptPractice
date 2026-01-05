4fefd3152a759bfa502659e6f1ce9242a464bbe5
Neel Prabir Singh <dyrokg@gmail.com>
2026-01-05 14:29:19 +0530

    5 jan

---
+++ 5-jan/script.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Bot1";
+let botLocation = "Mumbai";
+let favoroiteLanguage = "Javascript";
+
+console.log(`My Name is ${botName} and i live in ${botLocation}`);
+console.log(`My favoroite programming language is ${favoroiteLanguage}.`);
+
+
+let codingFact = `${favoroiteLanguage} is fun`;
+console.log(codingFact);
+
+codingFact = `${favoroiteLanguage} is very fun`
+console.log(codingFact)
+
+codingFact = `${favoroiteLanguage} is very very fun`
+console.log(codingFact)
+
+console.log(`It was fun sharing these facts with you. \n Goodbye! \n ${botName} \n from ${botLocation}`)
+
